0265c99690da32d0453d0fd45991f15e06544d71
Shar <81372629+shar27@users.noreply.github.com>
2024-09-29 17:23:49 +0200

    new section

--- src/screens/Landing.jsx
+++ src/screens/Landing.jsx
@@ -4,6 +4,7 @@ import TopNavbar from "../components/Nav/TopNavbar";
 import Header from "../components/Sections/Header";
 import Services from "../components/Sections/Services";
 import Projects from "../components/Sections/Projects";
+import Reviews from "../components/Sections/Reviews";
 import Locations from "../components/Sections/Locations";
 import Blog from "../components/Sections/Blog";
 import Contact from "../components/Sections/Contact";
@@ -15,6 +16,7 @@ export default function Landing() {
    
       <TopNavbar />
       <Header />
+      <Reviews/>
       <Services />
       <Locations/>
       {/* <Projects /> */}
